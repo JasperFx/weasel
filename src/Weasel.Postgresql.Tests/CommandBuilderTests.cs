2e3f8e02e9ea976c3b2ff4553b65b3343ae36c5d
jeremydmiller <jeremydmiller@yahoo.com>
2021-04-11 14:49:21 -0500

    Imported some CommandBuilder tests

---
+++ src/Weasel.Postgresql.Tests/CommandBuilderTests.cs
@@ -0,0 +1,53 @@
+using Npgsql;
+using Shouldly;
+using Xunit;
+
+namespace Weasel.Postgresql.Tests
+{
+    public class CommandBuilderTests
+    {
+
+        [Fact]
+        public void append_parameters_with_one_at_the_end()
+        {
+            var builder = new CommandBuilder(new NpgsqlCommand());
+
+            builder.Append("select data from table where ");
+            builder.AppendWithParameters("foo = ?")
+                .Length.ShouldBe(1);
+
+            builder.ToString().ShouldBe("select data from table where foo = :p0");
+
+
+        }
+
+        [Fact]
+        public void append_parameters_with_multiples_at_end()
+        {
+            var builder = new CommandBuilder(new NpgsqlCommand());
+
+            builder.Append("select data from table where ");
+            builder.AppendWithParameters("foo = ? and bar = ?")
+                .Length.ShouldBe(2);
+
+            builder.ToString().ShouldBe("select data from table where foo = :p0 and bar = :p1");
+
+
+        }
+
+
+        [Fact]
+        public void append_parameters_with_multiples_in_the_middle()
+        {
+            var builder = new CommandBuilder(new NpgsqlCommand());
+
+            builder.Append("select data from table where ");
+            builder.AppendWithParameters("foo = ? and bar = ? order by baz")
+                .Length.ShouldBe(2);
+
+            builder.ToString().ShouldBe("select data from table where foo = :p0 and bar = :p1 order by baz");
+
+
+        }
+    }
+}
