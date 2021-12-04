3b58c6f3cc0db3f929ab6130364e365f82ca6b44
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-12-04 12:24:56 +0100

    Bumped dependencies to major versions

--- src/Weasel.SqlServer.Tests/ConnectionSource.cs
+++ src/Weasel.SqlServer.Tests/ConnectionSource.cs
@@ -3,10 +3,10 @@ using Baseline;
 
 namespace Weasel.SqlServer.Tests
 {
-    public class ConnectionSource 
+    public class ConnectionSource
     {
         public static readonly string ConnectionString = Environment.GetEnvironmentVariable("weasel_sqlserver_testing_database")
-            ?? "Server=localhost;User Id=sa;Password=P@55w0rd;Timeout=5;";
+            ?? "Server=localhost;User Id=sa;Password=P@55w0rd;Timeout=5;TrustServerCertificate=true";
 
         static ConnectionSource()
         {
@@ -14,6 +14,6 @@ namespace Weasel.SqlServer.Tests
                 throw new Exception(
                     "You need to set the connection string for your local Postgresql database in the environment variable 'weasel_postgresql_testing_database'");
         }
-        
+
     }
 }
