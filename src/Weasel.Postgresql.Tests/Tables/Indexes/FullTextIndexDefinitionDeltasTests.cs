987c556473f6b2f023b13b28bdf0e19c4e16636a
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-12-05 11:51:24 +0100

    Added tests for FullTextIndex change detection
    Added common base class for index deltas detection tests

--- src/Weasel.Postgresql.Tests/Tables/Indexes/FullTextIndexDefinitionDeltasTests.cs
+++ src/Weasel.Postgresql.Tests/Tables/Indexes/FullTextIndexDefinitionDeltasTests.cs
@@ -1,245 +1,107 @@
-using Shouldly;
-using Weasel.Postgresql.Tables;
-using Weasel.Postgresql.Tables.Indexes;
+using Npgsql;
 using Xunit;
 
 namespace Weasel.Postgresql.Tests.Tables.Indexes;
 
-public class FullTextIndexDeltasTests: IntegrationContext
+[Collection("fts_deltas")]
+public class FullTextIndexDeltasTests(): IndexDeltasDetectionContext("fts_deltas", "users")
 {
-
-    public FullTextIndexDeltasTests(string schemaName) : base(schemaName)
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public Task wholedoc_fts_index_comparison_works()
     {
+        theTable.ModifyColumn("data").AddFullTextIndex();
+
+        return AssertNoDeltasAfterPatching();
+    }
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public Task fts_index_comparison_must_take_into_account_automatic_cast()
+    {
+        theTable.ModifyColumn("data").AddFullTextIndex(documentConfig: "(data ->> 'Name')");
+
+        return AssertNoDeltasAfterPatching();
+    }
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public Task multifield_fts_index_comparison_must_take_into_account_automatic_cast()
+    {
+        theTable.ModifyColumn("data")
+            .AddFullTextIndex(documentConfig: "((data ->> 'FirstName') || ' ' || (data ->> 'LastName'))");
+
+        return AssertNoDeltasAfterPatching();
+    }
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public async Task modified_fts_index_comparison_must_generate_index_update()
+    {
+        theTable.ModifyColumn("data").AddFullTextIndex(documentConfig: "(data ->> 'Name')");
+
+        await CreateSchemaObjectInDatabase(theTable);
+
+        theTable.Indexes.Clear();
+
+        theTable.ModifyColumn("data")
+            .AddFullTextIndex(documentConfig: "((data ->> 'FirstName') || ' ' || (data ->> 'LastName'))");
+
+        await AssertIndexUpdate($"{theTable.Identifier.Name}_idx_fts");
+    }
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public async Task modified_fts_index_by_regConfig_comparison_must_generate_index_update()
+    {
+        const string documentConfig = "(data ->> 'Name')";
+        theTable.ModifyColumn("data").AddFullTextIndex(documentConfig: documentConfig);
+
+        await CreateSchemaObjectInDatabase(theTable);
+
+        theTable.Indexes.Clear();
+
+        const string newRegConfig = "italian";
+        theTable.ModifyColumn("data")
+            .AddFullTextIndex(newRegConfig, documentConfig);
+
+        await AssertIndexRecreation(
+            $"{theTable.Identifier.Name}_{newRegConfig}_idx_fts",
+            $"{theTable.Identifier.Name}_idx_fts"
+        );
+    }
+
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public async Task modified_fts_index_with_customIndex_by_regConfig_comparison_must_generate_index_update()
+    {
+        const string documentConfig = "(data ->> 'Name')";
+        const string indexName = "custom_index_name";
+
+        theTable.ModifyColumn("data").AddFullTextIndex(documentConfig: documentConfig, indexName: indexName);
+
+        await CreateSchemaObjectInDatabase(theTable);
+
+        theTable.Indexes.Clear();
+
+        const string newRegConfig = "italian";
+        theTable.ModifyColumn("data")
+            .AddFullTextIndex(newRegConfig, documentConfig, indexName: indexName);
+
+        await AssertIndexUpdate(indexName);
+    }
+
+    [PgVersionTargetedFact(MinimumVersion = "10.0")]
+    public async Task migration_from_v3_to_v4_should_not_result_in_schema_difference()
+    {
+        await CreateSchemaObjectInDatabase(theTable);
+
+        // create index with a sql statement not containing `::regconfig`
+        await using (var conn = new NpgsqlConnection(ConnectionSource.ConnectionString))
+        {
+            await conn.OpenAsync();
+            await conn.CreateCommand(
+                    $"CREATE INDEX {theTable.Identifier.Name}_idx_fts ON {theTable.Identifier} USING gin (( to_tsvector('english', data) ))")
+                .ExecuteNonQueryAsync();
+        }
+
+        theTable.ModifyColumn("data").AddFullTextIndex();
+
+        await AssertNoDeltasAfterPatching();
     }
 }
-
-//
-//
-// #region sample_using_a_full_text_index_through_attribute_on_class_with_default
-//
-// [FullTextIndex]
-// public class Book
-// {
-//     public Guid Id { get; set; }
-//
-//     public string Title { get; set; }
-//
-//     public string Author { get; set; }
-//
-//     public string Information { get; set; }
-// }
-//
-// #endregion
-//
-// #region sample_using_a_single_property_full_text_index_through_attribute_with_default
-//
-// public class UserProfile
-// {
-//     public Guid Id { get; set; }
-//
-//     [FullTextIndex] public string Information { get; set; }
-// }
-//
-// #endregion
-//
-// #region sample_using_a_single_property_full_text_index_through_attribute_with_custom_settings
-//
-// public class UserDetails
-// {
-//     private const string FullTextIndexName = "mt_custom_user_details_fts_idx";
-//
-//     public Guid Id { get; set; }
-//
-//     [FullTextIndex(IndexName = FullTextIndexName, RegConfig = "italian")]
-//     public string Details { get; set; }
-// }
-//
-// #endregion
-//
-// #region sample_using_multiple_properties_full_text_index_through_attribute_with_default
-//
-// public class Article
-// {
-//     public Guid Id { get; set; }
-//
-//     [FullTextIndex] public string Heading { get; set; }
-//
-//     [FullTextIndex] public string Text { get; set; }
-// }
-//
-// #endregion
-//
-// #region sample_using_multiple_properties_full_text_index_through_attribute_with_custom_settings
-//
-// public class BlogPost
-// {
-//     public Guid Id { get; set; }
-//
-//     public string Category { get; set; }
-//
-//     [FullTextIndex] public string EnglishText { get; set; }
-//
-//     [FullTextIndex(RegConfig = "italian")] public string ItalianText { get; set; }
-//
-//     [FullTextIndex(RegConfig = "french")] public string FrenchText { get; set; }
-// }
-//
-// #endregion
-//
-// public class full_text_index: OneOffConfigurationsContext
-// {
-//     [PgVersionTargetedFact(MinimumVersion = "10.0")]
-//     public async Task wholedoc_fts_index_comparison_works()
-//     {
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<User>().FullTextIndex();
-//         });
-//
-//         // Apply changes
-//         await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
-//
-//         // Look at updates after that
-//         var patch = await theStore.Storage.Database.CreateMigrationAsync();
-//
-//         var patchSql = patch.UpdateSql();
-//
-//         Assert.DoesNotContain("drop index if exists full_text_index.mt_doc_user_idx_fts", patchSql);
-//         Assert.DoesNotContain("drop index full_text_index.mt_doc_user_idx_fts", patchSql);
-//     }
-//
-//     [PgVersionTargetedFact(MinimumVersion = "10.0")]
-//     public async Task fts_index_comparison_must_take_into_account_automatic_cast()
-//     {
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<Company>()
-//                 .FullTextIndex(x => x.Name);
-//         });
-//
-//         // Apply changes
-//         await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
-//
-//         // Look at updates after that
-//         var patch = await theStore.Storage.Database.CreateMigrationAsync();
-//
-//         var patchSql = patch.UpdateSql();
-//
-//         Assert.DoesNotContain("drop index if exists full_text_index.mt_doc_user_idx_fts", patchSql);
-//         Assert.DoesNotContain("drop index full_text_index.mt_doc_user_idx_fts", patchSql);
-//     }
-//
-//     [PgVersionTargetedFact(MinimumVersion = "10.0")]
-//     public async Task multifield_fts_index_comparison_must_take_into_account_automatic_cast()
-//     {
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<User>()
-//                 .FullTextIndex(x => x.FirstName, x => x.LastName);
-//         });
-//
-//         // Apply changes
-//         await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
-//
-//         // Look at updates after that
-//         var patch = await theStore.Storage.Database.CreateMigrationAsync();
-//
-//         var patchSql = patch.UpdateSql();
-//
-//         Assert.DoesNotContain("drop index if exists full_text_index.mt_doc_user_idx_fts", patchSql);
-//         Assert.DoesNotContain("drop index full_text_index.mt_doc_user_idx_fts", patchSql);
-//     }
-//
-//     [PgVersionTargetedFact(MinimumVersion = "10.0")]
-//     public async Task modified_fts_index_comparison_must_generate_drop()
-//     {
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<User>()
-//                 .FullTextIndex(x => x.FirstName);
-//         });
-//
-//         // Apply changes
-//         await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
-//
-//         // Change indexed fields
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<User>()
-//                 .FullTextIndex(x => x.FirstName, x => x.LastName);
-//         }, false);
-//
-//         // Look at updates after that
-//         var patch = await theStore.Storage.Database.CreateMigrationAsync();
-//
-//         Assert.Contains("drop index if exists full_text_index.mt_doc_user_idx_fts", patch.UpdateSql());
-//     }
-//
-//     [PgVersionTargetedFact(MinimumVersion = "10.0")]
-//     public async Task migration_from_v3_to_v4_should_not_result_in_schema_difference()
-//     {
-//         // setup/simulate a full text index as in v3
-//         StoreOptions(_ =>
-//         {
-//             _.Schema.For<User>().FullTextIndex();
-//         });
-//
-//         await theStore.Storage.ApplyAllConfiguredChangesToDatabaseAsync();
-//
-//         // drop and recreate index with a sql statement not containing `::regconfig`
-//         await using (var conn = new NpgsqlConnection(ConnectionSource.ConnectionString))
-//         {
-//             await conn.OpenAsync();
-//             await conn.CreateCommand("DROP INDEX if exists full_text_index.mt_doc_user_idx_fts")
-//                 .ExecuteNonQueryAsync();
-//             await conn.CreateCommand(
-//                     "CREATE INDEX mt_doc_user_idx_fts ON full_text_index.mt_doc_user USING gin (( to_tsvector('english', data) ))")
-//                 .ExecuteNonQueryAsync();
-//         }
-//
-//         // create another store and check if there is no schema difference
-//         var store2 = DocumentStore.For(_ =>
-//         {
-//             _.Connection(ConnectionSource.ConnectionString);
-//             _.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate;
-//             _.DatabaseSchemaName = "fulltext";
-//
-//             _.Schema.For<User>().FullTextIndex();
-//         });
-//         await Should.NotThrowAsync(async () => await store2.Storage.Database.AssertDatabaseMatchesConfigurationAsync());
-//     }
-// }
-//
-// public static class FullTextIndexTestsExtension
-// {
-//     public static void ShouldContainIndexDefinitionFor<TDocument>(
-//         this StorageFeatures storage,
-//         string tableName = "full_text_index.mt_doc_target",
-//         string indexName = "mt_doc_target_idx_fts",
-//         string regConfig = "english",
-//         string documentConfig = null)
-//     {
-//         var documentMapping = storage.MappingFor(typeof(TDocument));
-//         var table = new DocumentTable(documentMapping);
-//         var ddl = documentMapping.Indexes
-//             .Where(x => x.Name == indexName)
-//             .Select(x => x.ToDDL(table))
-//             .FirstOrDefault();
-//
-//         ddl.ShouldNotBeNull();
-//
-//         SpecificationExtensions.ShouldContain(ddl, $"CREATE INDEX {indexName}");
-//         SpecificationExtensions.ShouldContain(ddl, $"ON {tableName}");
-//         SpecificationExtensions.ShouldContain(ddl, $"to_tsvector('{regConfig}',{documentConfig})");
-//
-//         if (regConfig != null)
-//         {
-//             SpecificationExtensions.ShouldContain(ddl, regConfig);
-//         }
-//
-//         if (documentConfig != null)
-//         {
-//             SpecificationExtensions.ShouldContain(ddl, documentConfig);
-//         }
-//     }
-// }
---
+++ src/Weasel.Postgresql.Tests/Tables/Indexes/IndexDeltasDetectionContext.cs
@@ -0,0 +1,79 @@
+using Shouldly;
+using Weasel.Core;
+using Weasel.Postgresql.Tables;
+
+namespace Weasel.Postgresql.Tests.Tables.Indexes;
+
+public abstract class IndexDeltasDetectionContext: IntegrationContext
+{
+    protected Table theTable;
+
+    public override Task InitializeAsync() =>
+        ResetSchema();
+
+    protected IndexDeltasDetectionContext(string schemaName, string tableName = "people"): base(schemaName)
+    {
+        theTable = new Table($"{schemaName}.{tableName}");
+        theTable.AddColumn<int>("id").AsPrimaryKey();
+        theTable.AddColumn<string>("first_name");
+        theTable.AddColumn<string>("last_name");
+        theTable.AddColumn<string>("user_name");
+        theTable.AddColumn<DateTime>("created_datetime");
+        theTable.AddColumn<DateTimeOffset>("created_datetime_offset");
+        theTable.AddColumn("data", "jsonb");
+    }
+
+    protected async Task AssertNoDeltasAfterPatching(Table? table = null)
+    {
+        table ??= theTable;
+        await table.ApplyChangesAsync(theConnection);
+
+        var delta = await table.FindDeltaAsync(theConnection);
+
+        delta.HasChanges().ShouldBeFalse();
+    }
+
+    protected async Task<TableDelta> AssertIndexUpdate(
+        string indexName,
+        SchemaPatchDifference difference = SchemaPatchDifference.Update,
+        Table? table = null
+    )
+    {
+        var delta = await AssertIndexChange(difference, table);
+
+        delta.Indexes.Different.ShouldContain(
+            i => i.Expected.Name == indexName && i.Actual.Name == indexName
+        );
+
+        return delta;
+    }
+
+    protected async Task<TableDelta> AssertIndexRecreation(
+        string oldName,
+        string? indexName = null,
+        SchemaPatchDifference difference = SchemaPatchDifference.Update,
+        Table? table = null
+    )
+    {
+        indexName ??= oldName;
+
+        var delta = await AssertIndexChange(difference, table);
+
+        delta.Indexes.Extras.ShouldContain(i => i.Name == indexName);
+        delta.Indexes.Missing.ShouldContain(i => i.Name == oldName);
+
+        return delta;
+    }
+
+    private async Task<TableDelta> AssertIndexChange(SchemaPatchDifference difference, Table? table)
+    {
+        table ??= theTable;
+
+        var delta = await table.FindDeltaAsync(theConnection);
+
+        delta.HasChanges().ShouldBeTrue();
+
+        delta.Indexes.Difference().ShouldBe(difference);
+        return delta;
+    }
+}
