2d2839dca38649c7c910fc7168586d4bf877fd3e
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-12-06 12:13:44 +0100

    Brought back the previous NpgsqlConnection creation for IntegrationContext
    It seems that there's some issue (caching?) when multiple SQL statements are being called on changing database structure

--- src/Weasel.Postgresql.Tests/IntegrationContext.cs
+++ src/Weasel.Postgresql.Tests/IntegrationContext.cs
@@ -7,13 +7,13 @@ namespace Weasel.Postgresql.Tests;
 
 public abstract class IntegrationContext: IDisposable, IAsyncLifetime
 {
-    protected readonly NpgsqlDataSource theDataSource = NpgsqlDataSource.Create(ConnectionSource.ConnectionString);
+    //protected readonly NpgsqlDataSource theDataSource = NpgsqlDataSource.Create(ConnectionSource.ConnectionString);
     private NpgsqlConnection? connection;
 
-    protected NpgsqlConnection theConnection
-    {
-        get => (connection ??= theDataSource.CreateConnection());
-    }
+    protected NpgsqlConnection theConnection = new NpgsqlConnection(ConnectionSource.ConnectionString);
+    // {
+    //     get => (connection ??= theDataSource.CreateConnection());
+    // }
 
     protected IntegrationContext(string schemaName)
     {
@@ -30,8 +30,9 @@ public abstract class IntegrationContext: IDisposable, IAsyncLifetime
 
     public void Dispose()
     {
-        connection?.Dispose();
-        theDataSource.Dispose();
+        theConnection.Dispose();
+        //connection?.Dispose();
+        //theDataSource.Dispose();
     }
 
     protected async Task ResetSchema()
