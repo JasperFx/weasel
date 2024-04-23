9f0053647643306e6c3850f5ef516c37506ba424
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2024-04-23 16:40:17 +0200

    Add tests

--- src/Weasel.Postgresql.Tests/PostgresqlProviderTests.cs
+++ src/Weasel.Postgresql.Tests/PostgresqlProviderTests.cs
@@ -133,6 +133,112 @@ public class PostgresqlProviderTests
     {
         PostgresqlProvider.Instance.ConvertSynonyms(type).ShouldBe(synonym);
     }
+
+    [Fact]
+    public void execute_to_parameter_type_with_open_generics()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithOpenGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithOpenGeneric<>)).ShouldBe(NpgsqlDbType.Varchar);
+    }
+
+    [Fact]
+    public void execute_to_parameter_type_with_closed_generics_falls_back_to_open()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithOpenGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithOpenGeneric<int>)).ShouldBe(NpgsqlDbType.Varchar);
+    }
+
+    [Fact]
+    public void execute_to_parameter_type_with_closed_generics()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithClosedGeneric<int>)).ShouldBe(NpgsqlDbType.Varchar);
+    }
+
+    [Fact]
+    public void execute_to_parameter_type_with_closed_generics_open_generic()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        Action act = () => PostgresqlProvider.Instance.ToParameterType(typeof(WithClosedGeneric<>));
+        act.ShouldThrow<NotSupportedException>();
+    }
+
+    [Fact]
+    public void execute_to_parameter_type_with_closed_generics_generic_override()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        Action act = () => PostgresqlProvider.Instance.ToParameterType(typeof(WithClosedGeneric<string>));
+        act.ShouldThrow<NotSupportedException>();
+    }
+
+    [Fact]
+    public void execute_to_parameter_type_with_closed_generics_overrides_definitions_with_open_generic()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<int>), "integer", NpgsqlDbType.Integer);
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<bool>), "boolean", NpgsqlDbType.Boolean);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithMixedGeneric<int>)).ShouldBe(NpgsqlDbType.Integer);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithMixedGeneric<bool>)).ShouldBe(NpgsqlDbType.Boolean);
+        PostgresqlProvider.Instance.ToParameterType(typeof(WithMixedGeneric<string>)).ShouldBe(NpgsqlDbType.Varchar);
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_open_generics()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithOpenGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithOpenGeneric<>), EnumStorage.AsInteger).ShouldBe("varchar");
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_closed_generics_falls_back_to_open()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithOpenGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithOpenGeneric<int>), EnumStorage.AsInteger).ShouldBe("varchar");
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_closed_generics()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithClosedGeneric<int>), EnumStorage.AsInteger).ShouldBe("varchar");
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_closed_generics_open_generic()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithClosedGeneric<>), EnumStorage.AsInteger).ShouldBe("jsonb");
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_closed_generics_generic_override()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithClosedGeneric<int>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithClosedGeneric<string>), EnumStorage.AsInteger).ShouldBe("jsonb");
+    }
+
+    [Fact]
+    public void execute_to_db_type_with_closed_generics_overrides_definitions_with_open_generic()
+    {
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<>), "varchar", NpgsqlDbType.Varchar);
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<int>), "integer", NpgsqlDbType.Integer);
+        PostgresqlProvider.Instance.RegisterMapping(typeof(WithMixedGeneric<bool>), "boolean", NpgsqlDbType.Boolean);
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithMixedGeneric<int>), EnumStorage.AsInteger).ShouldBe("integer");
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithMixedGeneric<bool>), EnumStorage.AsInteger).ShouldBe("boolean");
+        PostgresqlProvider.Instance.GetDatabaseType(typeof(WithMixedGeneric<string>), EnumStorage.AsInteger).ShouldBe("varchar");
+    }
+
+    public class WithOpenGeneric<T>
+    {
+    }
+
+    public class WithClosedGeneric<T>
+    {
+    }
+
+    public class WithMixedGeneric<T>
+    {
+    }
 }
 
 [CollectionDefinition("PostgresqlProviderTests", DisableParallelization = true)]
