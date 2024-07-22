a04e2364d33a88051ba90bda69a18f163dd632d3
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-07-22 11:32:53 -0500

    Ability to generate working DDL to create tables with partitions

--- src/Weasel.Postgresql.Tests/Tables/creating_tables_with_partitions.cs
+++ src/Weasel.Postgresql.Tests/Tables/creating_tables_with_partitions.cs
@@ -27,6 +27,11 @@ public class creating_tables_with_partitions: IntegrationContext
 
         sql.ShouldContain("PARTITION BY LIST (role)");
 
+        await tryToCreateTable();
+    }
+
+    private async Task tryToCreateTable()
+    {
         await theConnection.OpenAsync();
 
         await theConnection.ResetSchemaAsync("partitions");
@@ -35,21 +40,60 @@ public class creating_tables_with_partitions: IntegrationContext
     }
 
     [Fact]
+    public async Task add_some_list_partitions()
+    {
+        theTable.PartitionByList("role");
+        theTable.AddListPartition("admin", "admin");
+        theTable.AddListPartition("super", "super");
+        theTable.AddListPartition("special", "special");
+
+        var sql = theTable.ToCreateSql(new PostgresqlMigrator());
+        sql.ShouldContain("partitions.people_admin");
+        sql.ShouldContain("partitions.people_super");
+        sql.ShouldContain("partitions.people_special");
+        sql.ShouldContain("partitions.people_default");
+
+        await tryToCreateTable();
+    }
+
+    [Fact]
     public async Task write_sql_for_partition_by_hash()
     {
-        theTable.PartitionByHash("role");
+        theTable.PartitionByHash(new HashPartitioning
+        {
+            Columns = ["role"],
+            Suffixes = ["one", "two", "three"]
+        });
 
         var sql = theTable.ToCreateSql(new PostgresqlMigrator());
 
         sql.ShouldContain("PARTITION BY HASH (role)");
 
-        await theConnection.OpenAsync();
+        sql.ShouldContain("create table partitions.people_one partition of partitions.people for values with (modulus 3, remainder 0);");
+        sql.ShouldContain("create table partitions.people_two partition of partitions.people for values with (modulus 3, remainder 1);");
+        sql.ShouldContain("create table partitions.people_three partition of partitions.people for values with (modulus 3, remainder 2);");
 
-        await theConnection.ResetSchemaAsync("partitions");
 
-        await theTable.MigrateAsync(theConnection);
+        await tryToCreateTable();
     }
 
+
+    [Fact]
+    public async Task add_range_partitions()
+    {
+        theTable.AddColumn<int>("age").AsPrimaryKey();
+        theTable.PartitionByRange("age");
+
+        theTable.AddRangePartition("twenties", 20, 29);
+        theTable.AddRangePartition("thirties", 30, 39);
+
+        var sql = theTable.ToCreateSql(new PostgresqlMigrator());
+        sql.ShouldContain("partitions.people_twenties");
+        sql.ShouldContain("partitions.people_thirties");
+        sql.ShouldContain("partitions.people_default");
+
+        await tryToCreateTable();
+    }
 
     [Fact]
     public async Task write_sql_for_partition_by_range()
@@ -60,10 +104,6 @@ public class creating_tables_with_partitions: IntegrationContext
 
         sql.ShouldContain("PARTITION BY RANGE (role)");
 
-        await theConnection.OpenAsync();
-
-        await theConnection.ResetSchemaAsync("partitions");
-
-        await theTable.MigrateAsync(theConnection);
+        await tryToCreateTable();
     }
 }
--- src/Weasel.Postgresql/Tables/IPartition.cs
+++ src/Weasel.Postgresql/Tables/IPartition.cs
@@ -1,3 +1,6 @@
+using JasperFx.Core;
+using JasperFx.Core.Reflection;
+
 namespace Weasel.Postgresql.Tables;
 
 
@@ -27,49 +30,95 @@ public enum PartitionStrategy
 
 public interface IPartition
 {
-    PartitionStrategy Strategy { get; }
+    string Suffix { get; }
+    void WriteCreateStatement(TextWriter writer, Table parent);
+}
 
-    string PartitionName { get; }
-    void WriteCreateStatement(Table parent);
+public class DefaultPartition: IPartition
+{
+    public string Suffix => "default";
+    public void WriteCreateStatement(TextWriter writer, Table parent)
+    {
+        writer.WriteLine($"CREATE TABLE {parent.Identifier}_default PARTITION OF {parent.Identifier} DEFAULT;");
+    }
 }
 
 public class ListPartition<T>: IPartition
 {
-    public ListPartition(string partitionName, params T[] values)
+    public ListPartition(string suffix, params T[] values)
     {
-        PartitionName = partitionName;
+        Suffix = suffix;
         Values = values;
     }
 
     public T[] Values { get; }
-    public string PartitionName { get; }
+    public string Suffix { get; }
 
-    public void WriteCreateStatement(Table parent)
+    public void WriteCreateStatement(TextWriter writer, Table parent)
     {
-        throw new NotImplementedException();
+        var values = Values.Select(x => x.ToString()).ToArray();
+        if (typeof(T) == typeof(string))
+        {
+            values = values.Select(x => $"'{x}'").ToArray();
+        }
+
+        writer.WriteLine($"CREATE TABLE {parent.Identifier}_{Suffix} partition of {parent.Identifier} for values in ({values.Join(", ")});");
     }
 
     public PartitionStrategy Strategy { get; }
 }
 
+public class HashPartitioning
+{
+    /// <summary>
+    /// The database columns to use as part of the hashing strategy
+    /// </summary>
+    public string[] Columns { get; init; }
+
+    /// <summary>
+    /// The suffix names for the partitioned table names. The modulo/remainder values
+    /// will be created automatically based on the number of suffixes
+    /// </summary>
+    public string[] Suffixes { get; init; }
+
+    internal IEnumerable<IPartition> BuildPartitions()
+    {
+        var modulus = Suffixes.Length;
+        var remainder = 0;
+        foreach (var suffix in Suffixes)
+        {
+            yield return new HashPartition(suffix, modulus, remainder);
+            remainder++;
+        }
+    }
+}
+
+public record HashPartition(string Suffix, int Modulus, int Remainder): IPartition
+{
+    public void WriteCreateStatement(TextWriter writer, Table parent)
+    {
+        writer.WriteLine($"create table {parent.Identifier}_{Suffix} partition of {parent.Identifier} for values with (modulus {Modulus}, remainder {Remainder});");
+    }
+}
+
 public class RangePartition<T>: IPartition
 {
-    public RangePartition(string partitionName, T from, T to)
+    public string Suffix { get; }
+    public T From { get; }
+    public T To { get; }
+
+    public RangePartition(string suffix, T from, T to)
     {
-        PartitionName = partitionName;
+        Suffix = suffix;
         From = from;
         To = to;
     }
 
-    public T From { get; }
-    public T To { get; }
-
-    public void WriteCreateStatement(Table parent)
+    public void WriteCreateStatement(TextWriter writer, Table parent)
     {
-        throw new NotImplementedException();
+        var from = typeof(T).IsNumeric() ? From.ToString() : $"'{From.ToString()}'";
+        var to = typeof(T).IsNumeric() ? To.ToString() : $"'{To.ToString()}'";
+
+        writer.WriteLine($"CREATE TABLE {parent.Identifier}_{Suffix} PARTITION OF {parent.Identifier} FOR VALUES FROM ({from}) TO ({to});");
     }
-
-    public PartitionStrategy Strategy { get; }
-
-    public string PartitionName { get; }
 }
