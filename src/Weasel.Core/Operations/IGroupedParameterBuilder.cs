33fbd4371821d1ffd987b5b4b22a9b977d23c8e5
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-04 14:39:49 -0600

    Thinned down IGroupedParameterBuilder quite a bit

--- src/Weasel.Core/Operations/IGroupedParameterBuilder.cs
+++ src/Weasel.Core/Operations/IGroupedParameterBuilder.cs
@@ -5,7 +5,6 @@ namespace Weasel.Core.Operations;
 public interface IGroupedParameterBuilder<TParameter, TDbType>
 {
     TParameter AppendParameter<T>(T? value) where T : notnull;
-    TParameter AppendParameter<T>(T? value, TDbType dbType) where T : notnull;
 
     void AppendJsonParameter(ISerializer serializer, object value);
 
