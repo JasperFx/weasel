48dd0c649aceab5dad5439bc1c01d1a05a14756b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-18 11:26:45 -0600

    Tweaking the SQL SErver CI build

--- src/Weasel.SqlServer.Tests/ConnectionSource.cs
+++ src/Weasel.SqlServer.Tests/ConnectionSource.cs
@@ -6,7 +6,7 @@ public class ConnectionSource
 {
     public static readonly string ConnectionString =
         Environment.GetEnvironmentVariable("weasel_sqlserver_testing_database")
-        ?? "Server=localhost,1434;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;Encrypt=False";
+        ?? "Server=localhost,1433;User Id=sa;Password=P@55w0rd;Timeout=5;MultipleActiveResultSets=True;Initial Catalog=master;Encrypt=False";
 
     static ConnectionSource()
     {
