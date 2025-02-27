4bd68eb497d526d979a45de962b8f3fe609c407f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-02-27 15:24:00 -0600

    helper function to drop a partition by value

--- src/Weasel.Postgresql/Tables/Partitioning/ManagedListPartitions.cs
+++ src/Weasel.Postgresql/Tables/Partitioning/ManagedListPartitions.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.Common;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using JasperFx.Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
