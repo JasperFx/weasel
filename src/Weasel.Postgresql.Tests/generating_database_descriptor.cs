80b6788822f7d2f3646778d2e3d7cfa7af29a8b8
JT <Hawxy@users.noreply.github.com>
2025-12-14 11:48:26 +0800

    Fix parsing of multihost connection strings

--- src/Weasel.Postgresql.Tests/generating_database_descriptor.cs
+++ src/Weasel.Postgresql.Tests/generating_database_descriptor.cs
@@ -22,6 +22,35 @@ public class generating_database_descriptor : IntegrationContext
         var descriptor = database.Describe();
 
         descriptor.ShouldNotBeNull();
+        descriptor.ServerName.ShouldBe("localhost");
+        descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("password")).ShouldBeFalse();
+        descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("certificate")).ShouldBeFalse();
+        descriptor.Properties.Any().ShouldBeTrue();
+    }
+
+    [Fact]
+    public void can_generate_the_descriptor_multihost()
+    {
+        var database = new DatabaseWithTables("Foo", NpgsqlDataSource.Create("Host=localhost:5432,localhost:5444;Database=marten_testing;Username=postgres;password=postgres;SSL Mode=Disable"));
+
+        var descriptor = database.Describe();
+
+        descriptor.ShouldNotBeNull();
+        descriptor.ServerName.ShouldBe("localhost");
+        descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("password")).ShouldBeFalse();
+        descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("certificate")).ShouldBeFalse();
+        descriptor.Properties.Any().ShouldBeTrue();
+    }
+
+    [Fact]
+    public void can_generate_the_descriptor_multihost_portlessdomains()
+    {
+        var database = new DatabaseWithTables("Foo", NpgsqlDataSource.Create("Host=my-db-host.com,my-db-host-ro.com;Database=marten_testing;Username=postgres;password=postgres;SSL Mode=Disable"));
+
+        var descriptor = database.Describe();
+
+        descriptor.ShouldNotBeNull();
+        descriptor.ServerName.ShouldBe("my-db-host.com");
         descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("password")).ShouldBeFalse();
         descriptor.Properties.Any(x => x.Name.ContainsIgnoreCase("certificate")).ShouldBeFalse();
         descriptor.Properties.Any().ShouldBeTrue();
