ce7d67b8a40443e67274b6d724290b6829088807
Jeremy D. Miller <jeremydmiller@yahoo.com>
2026-02-02 09:49:04 -0600

    added ITable and ITableColumn for later

--- src/Weasel.Core/ITableColumn.cs
+++ src/Weasel.Core/ITableColumn.cs
@@ -12,3 +12,17 @@ public interface ITableColumn : INamed
     string Type { get; set; }
     bool IsPrimaryKey { get; }
 }
+
+public interface ITable : ISchemaObject
+{
+    IReadOnlyList<string> PrimaryKeyColumns { get; }
+    string PrimaryKeyName { get; set; }
+    bool HasColumn(string columnName);
+    void RemoveColumn(string columnName);
+
+    ITableColumn AddColumn(string name, string columnType);
+    ITableColumn AddColumn(string name, Type dotnetType);
+
+    ITableColumn AddPrimaryKeyColumn(string name, string columnType);
+    ITableColumn AddPrimaryKeyColumn(string name, Type dotnetType);
+}
