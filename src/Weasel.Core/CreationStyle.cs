96aa4aa9a2fcfa7c8b5804ad2b53d14242e0fcfc
Kristoffer Strube <kristoffer.strube@gmail.com>
2026-02-12 15:03:14 +0100

    Align summary of `CreationStyle` with change made in version 8.5.0

--- src/Weasel.Core/CreationStyle.cs
+++ src/Weasel.Core/CreationStyle.cs
@@ -3,12 +3,12 @@
 public enum CreationStyle
 {
     /// <summary>
-    ///     Export DDL by first issuing a DROP statement for a table, then the CREATE statement. This is the default
+    ///     Export DDL by first issuing a DROP statement for a table, then the CREATE statement.
     /// </summary>
     DropThenCreate,
 
     /// <summary>
-    ///     Export DDL for table creation by using a CREATE IF NOT EXISTS clause w/o a prior DROP statement
+    ///     Export DDL for table creation by using a CREATE IF NOT EXISTS clause w/o a prior DROP statement. This is the default.
     /// </summary>
     CreateIfNotExists
 }
